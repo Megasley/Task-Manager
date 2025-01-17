f8d244635dc83694012742556a19a788d93f8471
Megasley <108496069+Megasley@users.noreply.github.com>
2025-01-17 15:18:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ USER_ID = {
     'Megasley': os.environ.get('USER_ID_MEGASLEY'),
     'Yami': os.environ.get('USER_ID_YAMI'),
     'Satoshee': os.environ.get('USER_ID_SATOSHEE'),
-    'EVerything Satoshi': os.environ.get('USER_ID_ES'),
+    'EVerything Satoshee': os.environ.get('USER_ID_ES'),
     'Lys': os.environ.get('USER_ID_LYS'),
     'Vviey': os.environ.get('USER_ID_VVY'),
 }
