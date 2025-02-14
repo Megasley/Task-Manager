4549e97f9950a28c4aad33c01fed3216d61a88aa
megasley <megasley@gmail.com>
2025-02-14 14:22:08 +0100

    Fixed Role tagging

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ def happenings():
 **Notable Happenings of The Week** 📝
 
 {events_list}
-<@1296146849517928470>
+<@&1296146849517928470>
     """
     asyncio.run_coroutine_threadsafe(send_to_discord(message), bot.loop)
 
